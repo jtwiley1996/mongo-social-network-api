2fb1d2f0823e0d7af5dcf0fe24502335d17a9e0a
jtwiley1996 <98716163+jtwiley1996@users.noreply.github.com>
2024-04-12 12:37:53 -0500

    added part of readme.md

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # mongo-social-network-api
+
+# Social-Network-API
+
+![Static Badge](https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&labelColor=black)
+![Static Badge](https://img.shields.io/badge/mongodb%20-%20%2347A248?style=for-the-badge&logo=mongodb&labelColor=black)
+![Static Badge](https://img.shields.io/badge/Express%20-%20black?style=for-the-badge&logo=Express&logoColor=white&labelColor=black&color=white)
+![Static Badge](https://img.shields.io/badge/Node.js-%23339933?style=for-the-badge&logo=node.js&labelColor=black)
+![Static Badge](https://img.shields.io/badge/insomnia%20-%20%234000BF?style=for-the-badge&logo=insomnia&labelColor=black)
+
+## Description
+My Social Network API is a project designed to explore network API workings using MongoDB. It's inspired by a vision of creating a platform for users to freely share thoughts, connect with friends, and express reactions. This endeavor has deepened my understanding of API design, CRUD operations, reaction and friendship management, and MongoDB's data modeling, system architecture, and user-centric design principles. It's been a valuable learning experience that underscores the significance of databases in real-world applications.
+
+## Installation
+
+You can interact with this app in two ways:
+
+1. To view a video demonstration, visit: [Demo Video](https://drive.google.com/file/d/1EN5D0AKnNgdLaj7cyflOsOnx9xGu-g6P/view)
+2. To run the app locally, ensure you have Node.js installed. Then clone the repository and follow the steps below:
+
+```bash
+npm i
+npm start
--- index.js
+++ index.js
@@ -1,40 +1,33 @@
 const express = require('express');
 const db = require('./config/connection');
-const routes = require('./routes');
+const apiRoutes = require('./routes/apis');
 const cfonts = require('cfonts');
-
-const cwd = process.cwd();
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-const activity = cwd.includes('mongo-social-network-api')
-  ? cwd.split('mongo-social-network-api')[1]
-  : cwd;
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(routes);
+app.use(apiRoutes);
 
-
-//Function that shows the title of the app
+// Generate and display ASCII art text
 cfonts.say('Social Network API', {
-	font: 'block',              // define the font face
-	align: 'center',              // define text alignment
-	colors: ['white'],         // define all colors
-	background: 'transparent',  // define the background color, you can also use `backgroundColor` here as key
-	letterSpacing: 1,           // define letter spacing
-	lineHeight: 1,              // define the line height
-	space: true,                // define if the output text should have empty lines on top and on the bottom
-	maxLength: '0',             // define how many character can be on one line
-	gradient: false,            // define your two gradient colors
-	independentGradient: false, // define if you want to recalculate the gradient for each new line
-	transitionGradient: false,  // define if this is a transition between colors directly
-	env: 'node'                 // define the environment cfonts is being executed in
+  font: 'block',             
+  align: 'center',             
+  colors: ['white'],        
+  background: 'transparent',  
+  letterSpacing: 1,          
+  lineHeight: 1,             
+  space: true,               
+  maxLength: '0',            
+  gradient: false,           
+  independentGradient: false, 
+  transitionGradient: false,  
+  env: 'node'                 
 });
 
 db.once('open', () => {
   app.listen(PORT, () => {
-    console.log(`API server for ${activity} running on port ${PORT}!`);
+    console.log(`API server running on port ${PORT}!`);
   });
 });
