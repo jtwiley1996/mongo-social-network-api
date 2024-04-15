f3e5f381f5cec4f0b573d1e6a06bc64ba0549dd2
jtwiley1996 <98716163+jtwiley1996@users.noreply.github.com>
2024-04-15 18:27:38 -0500

    made it look more fun

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const db = require('./config/connection');
-const apiRoutes = require('./routes/apis');
+const routes = require('./routes/');
 const cfonts = require('cfonts');
 
 const PORT = process.env.PORT || 3001;
@@ -8,23 +8,25 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(apiRoutes);
+app.use(routes);
 
 // Generate and display ASCII art text
-cfonts.say('Social Network API', {
-  font: 'block',             
-  align: 'center',             
-  colors: ['white'],        
-  background: 'transparent',  
-  letterSpacing: 1,          
-  lineHeight: 1,             
-  space: true,               
-  maxLength: '0',            
-  gradient: false,           
-  independentGradient: false, 
-  transitionGradient: false,  
-  env: 'node'                 
+cfonts.say('Social Network', {
+  font: 'block',
+  align: 'center',
+  colors: ['cyanBright', 'magentaBright', 'yellowBright', 'greenBright', 'blueBright'], // Bright colors
+  background: 'transparent',
+  letterSpacing: 1,
+  lineHeight: 1,
+  space: true,
+  maxLength: '0',
+  gradient: true, // Apply gradient
+  independentGradient: false,
+  transitionGradient: false,
+  env: 'node'
 });
+
+
 
 db.once('open', () => {
   app.listen(PORT, () => {
